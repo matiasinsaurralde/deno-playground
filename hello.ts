0fd032838bdba26d08b76f32bff58fef8bd3207c
Matias Insaurralde <matias@insaurral.de>
2022-06-23 17:57:09 -0400

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -1,11 +1,3 @@
-import { serve } from "std/http/server.ts";
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 
-async function handler(_req: Request) {
-  const text = await Deno.readTextFile(new URL(import.meta.url));
-  return new Response(text, {
-    headers: { "content-type": "text/plain; charset=utf8" },
-  });
-}
-
-console.log("Listening on http://localhost:8000");
-serve(handler);
+serve((req: Request) => new Response("Hello World"));
